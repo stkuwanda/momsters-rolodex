0ffe6a829e8d230e334945aa3e0a28b827e26988
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2021-12-14 08:30:10 +0200

    fetched users from api and displayed users/monsters

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = [
-      { name: "Frankenstein", id: 1 },
-      { name: "Dracula", id: 2 },
-      { name: "Zombie", id: 3 },
-    ];
+    this.state = { monsters: [] };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => {console.log('Response: ', res); return res.json()})
-    .then(res => console.log(res))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log("Response: ", res);
+        return res.json();
+      })
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className='App'>
-        {this.state.map((monster) => (
+        {this.state.monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
       </div>
