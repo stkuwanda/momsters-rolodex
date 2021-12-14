8d1c905b939c920a95fa0253c891780c26fa94f6
Simbarashe Tanaka Kuwanda <stkuwanda@gmail.com>
2021-12-15 01:42:17 +0200

    filtered monsters in search input, converted seaarch input to search-box component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
-import { CardList } from "./components/card-list/card-list.component";
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { monsters: [] };
+    this.state = { monsters: [], searchField: '' };
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
-        console.log("Response: ", res);
+        console.log('Response: ', res);
         return res.json();
       })
       .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder='search monster'
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
